baf68723235601ad57539d9c1b71f81ee06e07aa
Toma <tomattka@gmail.com>
2021-05-07 08:13:29 +0300

    Login form mobile and scripts, login form finished

--- html_new/script/popups.js
+++ html_new/script/popups.js
@@ -1,15 +1,3 @@
-function closeAll(){
-    $('#darkscreen').children().hide();
-    $('#darkscreen').hide();
-}
-
-function showLoginForm(){
-    $('#darkscreen').css('display','flex');
-    $('#login-form').show();
-    return false;
-}
-
-
 $(document).ready(function(){
 
     // Login form show
@@ -18,8 +6,37 @@ $(document).ready(function(){
 
     // Forms close functions
     $('#darkscreen').click(closeAll);
+    $('#popups').click(closeAll);
     $('#login-form-close').click(closeAll);
     $('#login-form').click(function(e){
         e.stopPropagation();
     });
+
+    $('#bAddFriendMobile').click(showLoginForm); // !!! temp
 });
+
+function closeAll(){
+    $('#popups').children().hide();
+    $('#popups').hide();
+    $('#darkscreen').hide();
+}
+
+function showLoginForm(){
+    $('#darkscreen').show();
+    $('#popups').css('display','flex');
+    $('#login-form').show();
+    scrollTopMobile();
+    return false;
+}
+
+function scrollTop(event, speed=400) {
+    event.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, speed);
+}
+
+function scrollTopMobile(){
+    windowWidth = $(window).width();
+    if (windowWidth < 767)
+
+    $('html, body').animate({ scrollTop: 0 }, 400);
+}
