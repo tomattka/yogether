00b0ffcf7b1c50dcf3759bfec902ccd87e253c87
Toma <tomattka@gmail.com>
2021-05-08 14:45:14 +0300

    mobile menu alnost done (has a bug)

--- html_new/script/popups.js
+++ html_new/script/popups.js
@@ -1,29 +1,49 @@
+var mobMenu, popups, darkscreen;
+
 $(document).ready(function(){
 
+    // Assigning elems to variables
+    mobMenu = $("#mobile-menu");
+    popups = $("#popups");
+    darkscreen = $('#darkscreen');
+
+    // --------- Mobile menu ---------- //
+    $("#mobile-menu-close").click(closeMenu); // menu x button
+    $("#aMobMenu").click(showMenu); // menu icon click
+
     // Login form show
-    $('#a-login').click(showLoginForm);
-    $('#b-register').click(showLoginForm); // !!! temp
+    $('#aLogin').click(showLoginForm);
+    $('#aLoginMob').click(showLoginForm);
+    $('#bRegister').click(showLoginForm); // !!! temp
 
     // Forms close functions
-    $('#darkscreen').click(closeAll);
-    $('#popups').click(closeAll);
+    darkscreen.click(closeAll);
+    popups.click(closeAll);
     $('#login-form-close').click(closeAll);
     $('#login-form').click(function(e){
         e.stopPropagation();
     });
 
-    $('#bAddFriendMobile').click(showLoginForm); // !!! temp
 });
 
+$(window).resize(moveMenu); // correcting menu position on window resize
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!! Здесь в попапс перепродумать логику
+
+// --------------------- Popups ----------------------- //
 function closeAll(){
-    $('#popups').children().hide();
-    $('#popups').hide();
-    $('#darkscreen').hide();
+    if (mobMenu.is(":visible"))
+        closeMenu();
+    else{
+    popups.children().hide();
+    popups.hide();
+    darkscreen.hide();
+    }
 }
 
 function showLoginForm(){
-    $('#darkscreen').show();
-    $('#popups').css('display','flex');
+    darkscreen.show();
+    popups.css('display','flex');
     $('#login-form').show();
     scrollTopMobile();
     return false;
@@ -39,4 +59,42 @@ function scrollTopMobile(){
     if (windowWidth < 767)
 
     $('html, body').animate({ scrollTop: 0 }, 400);
+}
+
+// ----------------- Mobile menu ------------------- //
+
+
+function showMenu(){
+    darkscreen.show();
+    popups.css('display','flex');
+    checkMenuHeight();
+    mobMenu.animate({left: "-=" + mobMenu.width()}, 350);
+    return false;
+}
+
+function closeMenu(){
+    mobMenu.animate({left: "+=" + mobMenu.width()}, 350, 
+    function(){ // menu animate end
+        popups.hide();
+        darkscreen.hide();
+    });
+    return false;
+}
+
+function moveMenu(){ // correcting menu position on window resize
+    checkMenuHeight();
+    if (mobMenu.is(":visible"))
+    {
+        leftPos = $(window).width() - mobMenu.width();
+        mobMenu.css('left', leftPos + 'px');
+    }
+    else
+        mobMenu.css('left', $(window).width() + 'px');
+}
+
+function checkMenuHeight(){
+    if (mobMenu.height() < $(window).height())
+        popups.css('height', '100%');
+    if (popups.height() < mobMenu.height())
+        popups.css('height', mobMenu.height() + 'px');
 }
